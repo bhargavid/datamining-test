01269d2c07472a50a9a731911c1ac048dcec0631
kewlb <kewlb@outlook.com>
2014-09-14 21:14:36 -0700

    Add output folder & fixed percentage problem

--- src/main/java/com/mstest/datamining/utils/FileUtil.java
+++ src/main/java/com/mstest/datamining/utils/FileUtil.java
@@ -14,14 +14,16 @@ import com.mstest.datamining.model.Graph;
 public class FileUtil {    
     
     private static final String FS = " ";
+    private static final String NEW_LINE = "\n";
+    
     public static File createDatFile(Graph graph, String graph_name) throws IOException {
         File tmpFile = File.createTempFile(graph_name, ".dat");
         
         StringBuilder sb = new StringBuilder();
-        sb.append(graph.getXAxis()).append(FS).append(graph.getY1Axis()).append(FS).append(graph.getY2Axis());
+        sb.append(graph.getXAxis()).append(FS).append(graph.getY1Axis()).append(FS).append(graph.getY2Axis()).append(NEW_LINE);
         
         for(Axis axis: graph.getAxisList()) {
-            sb.append(axis.getX()).append(FS).append(axis.getY1()).append(FS).append(axis.getY2());
+            sb.append(axis.getX()).append(FS).append(axis.getY1()).append(FS).append(axis.getY2()).append(NEW_LINE);
         }
         
         FileWriter fw = new FileWriter(tmpFile.getAbsoluteFile());
